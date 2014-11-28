89d02af84bb5b15f254fe8a77efabd3dc20f0272
Michael Louie Loria <michaellouieloria@gmail.com>
2014-11-28 15:55:15 +0800

    image upload

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,11 +1,22 @@
 Template.postSubmit.events({
-  'submit form': function(e) {
+  'submit form': function(e, template) {
     e.preventDefault();
     var post = {
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val(),
-      image: $(e.target).find('[name=image]').val()
+      image: 'sdsdsd'
     };
+
+
+      var input = template.find('input[type=file]');
+      var files = input.files;
+
+console.log(files[0]);
+Meteor.saveFile(files[0], 'lorz.jpg', '', 'binary');
+    // _.each(e.srcElement.files, function(file) {
+    //   console.log(file.name);
+    //   Meteor.saveFile(file, file.name);
+    // });
 
     Meteor.call('postInsert', post, function(error, result) {
     // display the error to the user and abort
@@ -17,6 +28,7 @@ Template.postSubmit.events({
         //Alerts.add('Database reading error!');
 
       //Router.go('postPage', {_id: result._id});
+
       Router.go('postsList');
     });
   }
@@ -27,3 +39,27 @@ Template.uploadInfo.helpers({
     return Uploader.info.get();
   }
 });
+
+Meteor.saveFile = function(blob, name, path, type) {
+  var fileReader = new FileReader(),
+    method, encoding = 'binary', type = type || 'binary';
+  switch (type) {
+    case 'text':
+      // TODO Is this needed? If we're uploading content from file, yes, but if it's from an input/textarea I think not...
+      method = 'readAsText';
+      encoding = 'utf8';
+      break;
+    case 'binary':
+      method = 'readAsBinaryString';
+      encoding = 'binary';
+      break;
+    default:
+      method = 'readAsBinaryString';
+      encoding = 'binary';
+      break;
+  }
+  fileReader.onload = function(file) {
+    Meteor.call('saveFile', file.srcElement.result, name, path, encoding, function(error, result) {});
+  }
+  fileReader[method](blob);
+}
--- server/int.js
+++ server/int.js
@@ -1,14 +1,58 @@
 Meteor.startup(function () {
   UploadServer.init({
     tmpDir: process.env.PWD + '/.uploads/tmp',
-    uploadDir: process.env.PWD + '/public/uploads/',
+    uploadDir: process.env.PWD + '/.uploads',
     getDirectory: function(file, formData) {
       return formData.contentType;
+    },
+    getFileName: function(file, formData) {
+      var trimmed = file.trim();
+      slug = Random.id() + '-' + trimmed.replace(/[^a-z0-9-.]/gi, '-').replace(/-+/g, '-').replace(/^-|-$/g, '');
+
+      return slug + file;
     },
     finished: function(file, folder, formFields) {
       console.log('Write to database: ' + folder + '/' + file);
       console.log(formFields);
-      //Session.set("image", folder + '/' + file);
     }
-  })
+  });
+
+  fs = Npm.require('fs');
+});
+
+Meteor.methods({
+  saveFile: function(blob, name, path, encoding) {
+    console.log('sdsd');
+    check(blob, Match.Any);
+    check(name, String);
+    check(path, String);
+    check(encoding, String);
+
+    var path = cleanPath(path),
+      name = cleanName(name || 'file'), encoding = encoding || 'binary',
+      chroot = Meteor.chroot || (process.env.PWD + '/.uploads/images');
+    // Clean up the path. Remove any initial and final '/' -we prefix them-,
+    // any sort of attempt to go to the parent directory '..' and any empty directories in
+    // between '/////' - which may happen after removing '..'
+    path = chroot + (path ? '/' + path + '/' : '/');
+
+    // TODO Add file existance checks, etc...
+    fs.writeFile(path + name, blob, encoding, function(err) {
+      if (err) {
+        throw (new Meteor.Error(500, 'Failed to save file.', err));
+      } else {
+        console.log('The file ' + name + ' (' + encoding + ') was saved to ' + path);
+      }
+    });
+
+    function cleanPath(str) {
+      if (str) {
+        return str.replace(/\.\./g,'').replace(/\/+/g,'').
+          replace(/^\/+/,'').replace(/\/+$/,'');
+      }
+    }
+    function cleanName(str) {
+      return str.replace(/\.\./g,'').replace(/\//g,'');
+    }
+  }
 });
